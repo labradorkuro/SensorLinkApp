c2c652503b239665cab601611a3ddff495868588
Takenori Tanaka <jazzsaxplayer02@gmail.com>
2015-08-07 16:43:36 +0900

    Initial Commit

---
+++ SensorLinkApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SensorLinkApp
+//
+//  Created by TanakaTakenori on 2015/08/07.
+//  Copyright (c) 2015年 TanakaTakenori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
