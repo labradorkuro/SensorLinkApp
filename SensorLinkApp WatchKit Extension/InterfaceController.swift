67f5b439e2da9c9b36e0442db2cb85da7bb5f01b
Takenori Tanaka <jazzsaxplayer02@gmail.com>
2015-09-10 18:21:50 +0900

    2015_09_10

--- SensorLinkApp WatchKit Extension/InterfaceController.swift
+++ SensorLinkApp WatchKit Extension/InterfaceController.swift
@@ -12,6 +12,7 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
 
+    @IBOutlet var measureDate: WKInterfaceLabel!
     @IBOutlet weak var sensorValue_1: WKInterfaceLabel!
     @IBOutlet weak var sensorValue_2: WKInterfaceLabel!
     @IBOutlet weak var measureTime: WKInterfaceLabel!
@@ -27,11 +28,18 @@ class InterfaceController: WKInterfaceController {
         super.willActivate()
         setSensorValue_1(10.00)
         setSensorValue_2(25.00)
+        updateView()
     }
 
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
+    }
+    func updateMeasureDate(date:String?) {
+        measureDate.setText("計測日付　" + date!)
+    }
+    func updateMeasureTime(time:String?) {
+        measureTime.setText("計測時間　" + time!)
     }
     // 電流値の表示更新
     func setSensorValue_1(val:Float?) {
@@ -42,5 +50,13 @@ class InterfaceController: WKInterfaceController {
     func setSensorValue_2(val:Float?) {
         sensorValue_2.setText("温度値　" +
             (NSString(format:"%.2f",val!) as String) + "℃")
+    }
+    // 表示の更新ボタンアクション
+    @IBAction func updateView() {
+        let dataDef : NSUserDefaults = NSUserDefaults(suiteName : "group.com.niigata-sl.com.SensorLinkApp")!
+        updateMeasureDate(dataDef.stringForKey("measureDate"))
+        updateMeasureTime(dataDef.stringForKey("measureTime"))
+        setSensorValue_1(dataDef.floatForKey("ampValue"))
+        setSensorValue_2(dataDef.floatForKey("tempValue"))
     }
 }
--- SensorLinkApp/ViewController.swift
+++ SensorLinkApp/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var measureDate: UILabel!
+    @IBOutlet weak var measureTime: UILabel!
+    @IBOutlet weak var ampValue: UILabel!
+    @IBOutlet weak var tempValue: UILabel!
+    
+    var timer: NSTimer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // サーバへデータをリクエスト
+        sendRequest()
+        // タイマー設定。30秒ごとにサーバへデータをリクエストして表示を更新する
+        timer = NSTimer.scheduledTimerWithTimeInterval(30, target: self, selector: "sendRequest", userInfo: nil, repeats: true)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,81 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func requestSend(sender: UIButton) {
+        sendRequest()
+    }
+    
+    // サーバーへデータを要求する
+    func sendRequest() {
+        var startDate : String
+        
+        // 現在日時
+        let today = NSDate()
+        // 日付フォーマット設定
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.locale = NSLocale(localeIdentifier: "ja_JP")
+        dateFormatter.timeStyle = .MediumStyle
+        dateFormatter.dateStyle = .MediumStyle
+        startDate = dateFormatter.stringFromDate(today)
+        //startDate = (startDate as NSString).substringWithRange(NSRange(location: 0, length: 10))
+        // 日付部分文字列取得
+        let startIndex = startDate.startIndex;
+        startDate = startDate.substringToIndex(startIndex.advancedBy(10))
+        startDate = startDate.stringByReplacingOccurrencesOfString("/", withString: "", range: nil)
+        // NSURLConnectionでWebAPIを呼び出す
+        let myUrl = NSURL(string: "http://157.7.222.217/trend_get?startdate=" + startDate + "&enddate=" + startDate + "&interval=1")
+        let req = NSURLRequest(URL: myUrl!)
+        //let connection = NSURLConnection(request: req, delegate: self, startImmediately: false)
+        NSURLConnection.sendAsynchronousRequest(req, queue: NSOperationQueue.mainQueue(), completionHandler: self.fetchResponse)
+        
+    }
+    // WebAPIの呼び出しresponse処理
+    func fetchResponse(res:NSURLResponse?, data:NSData?, error:NSError?) {
+        var date = "2015/09/01"
+        var time = "12:00:00"
+        var amp = "15.0"
+        var temp = "21.5"
+        if data != nil {
+        do {
+            let json = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.AllowFragments) as! Dictionary<String,AnyObject>
+            if let last_trend: Dictionary = json["last_trend"] as? Dictionary<String,AnyObject> {
+                date = last_trend["date"] as! String
+                time = last_trend["time"] as! String
+                if let values: Array = last_trend["value"] as? Array<String> {
+                    amp = values[0] 
+                    temp = values[1]
+                }
+            }
+            
+        }
+        catch {
+            
+        }
+        let dataDef : NSUserDefaults = NSUserDefaults(suiteName: "group.com.niigata-sl.com.SensorLinkApp")!
 
+        dataDef.setObject(date, forKey: "measureDate")
+        dataDef.setObject(time, forKey: "measureTime")
+        dataDef.setObject(amp, forKey: "ampValue")
+        dataDef.setObject(temp, forKey: "tempValue")
+        
+        updateDateLabel(dataDef.stringForKey("measureDate")!)
+        updateTimeLabel(dataDef.stringForKey("measureTime")!)
+        updateAmpLabel(dataDef.floatForKey("ampValue"))
+        updateTempLabel(dataDef.floatForKey("tempValue"))
+        }
+    }
+    func updateDateLabel(val:NSString) {
+        measureDate.text =  "計測日付　" + (val as String)
+    }
+    func updateTimeLabel(val:NSString) {
+        measureTime.text =  "計測時間　" + (val as String)
+    }
+
+    func updateAmpLabel(val:Float) {
+        ampValue.text = "電流値 " + (NSString(format:"%.2f",val) as String) + "mA"
+    }
+    func updateTempLabel(val:Float) {
+        tempValue.text = "温度値 " + (NSString(format:"%.2f",val) as String) + "℃"
+    }
 }
 
